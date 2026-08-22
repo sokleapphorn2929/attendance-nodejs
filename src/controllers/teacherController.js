56e01b0878004080def1ecad107ac59d92574766
SOKLEAP <phornzokl3ap@gmail.com>
2026-08-22 16:35:03 +0700

    done register login logout

--- src/controllers/teacherController.js
+++ src/controllers/teacherController.js
@@ -43,3 +43,89 @@ export const registerTeacher = async (req, res) => {
         )
     }
 }
+
+export const loginTeacher = async (req, res) => {
+    try {
+        const {username, password} = req.body;
+
+        const teacher = await Teacher.findOne(
+            {
+                where: {username}
+            }
+        )
+        if(!teacher){
+            return res.status(400).json(
+                {
+                    message: 'Invalid username or password.'
+                }
+            )
+        }
+
+        const validPassword = await bcrypt.compare(password, teacher.password);
+        if(!validPassword){
+            return res.status(400).json(
+                {
+                    message: 'Invalid username or password.'
+                }
+            )
+        }
+
+        const token = jwt.sign(
+            {
+                id: teacher.id,
+                username: teacher.username
+            },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: '7d'
+            }
+        )
+
+        return res.status(200).json(
+            {
+                message: 'Login successful.',
+                token,
+                teacher: {
+                    id: teacher.id,
+                    username: teacher.username
+                }
+            }
+        )
+    } catch (error) {
+        res.status(500).json(
+            {
+                message: 'Internal server error.',
+                error: error.message
+            }
+        )
+    }
+}
+
+export const logoutTeacher = async (req, res) => {
+    try {
+        const token = req.headers.authorization?.split(' ')[1];
+
+        if(!token){
+            return res.status(401).json(
+                {
+                    message: 'Unauthorized.'
+                }
+            )
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        
+        return res.status(200).json(
+            {
+                message: 'Logout successful.',
+            }
+        )
+    } catch (error) {
+        res.status(500).json(
+            {
+                message: 'Internal server error.',
+                error: error.message
+            }
+        )
+    }
+}
--- src/routes/apiRoute.js
+++ src/routes/apiRoute.js
@@ -4,5 +4,7 @@ import * as teacherController from '../controllers/teacherController.js';
 const router = express.Router();
 
 router.post('/register', teacherController.registerTeacher);
+router.post('/login', teacherController.loginTeacher);
+router.post('/logout', teacherController.logoutTeacher);
 
 export default router;
